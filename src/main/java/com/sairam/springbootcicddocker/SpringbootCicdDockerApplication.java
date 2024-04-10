9fe6d0e8b838dfbd6721a25835252b0be326b9a7
Sai Ram <sairammojjada82@gmail.com>
2024-04-10 14:27:15 +0530

    initial commit after setup

---
+++ src/main/java/com/sairam/springbootcicddocker/SpringbootCicdDockerApplication.java
@@ -0,0 +1,22 @@
+package com.sairam.springbootcicddocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootCicdDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdDockerApplication.class, args);
+	}
+
+	@GetMapping("/about")
+	public String getAbout(@RequestParam String info){
+		return "Responding from docker container "+info;
+	}
+}
